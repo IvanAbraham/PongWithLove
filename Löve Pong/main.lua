40f0a123c1fea8a0a8738fd17fbbedd6b843ea60
IvanAbraham <ivan99.abraham@gmail.com>
2023-09-29 12:56:24 -0300

    Added pallets for both player one and two

--- Löve Pong/main.lua
+++ Löve Pong/main.lua
@@ -1,31 +1,50 @@
 function love.load()
    
-  love.window.setTitle("Ejemplo")
-  
-  love.graphics.setBackgroundColor(1, 1, 1)
-  spaceshipImage = love.graphics.newImage("spaceship.png")
-  
-  posX = 0
-  posY = 0
-
-  speed = 10.0
-
+  pallet1 = 
+  {
+    x = 750,
+    y = 50,
+    width = 10,
+    height = 150,
+    speed = 10
+  }
+    
+  pallet2 =
+  {
+    x = 50,
+    y = 50,
+    width = 10,
+    height = 150,
+    speed = 10
+  }
+   
 end
 
 
 function love.update(dt)
   
-  posX = posX + speed * dt
-  posY = posY + speed * dt
-  
-  print(posX)
-  print(posY)
-  
-end
+  if love.keyboard.isDown("up") then
+    pallet1.y = pallet1.y - pallet1.speed
+    
+  elseif love.keyboard.isDown("down") then
+    pallet1.y = pallet1.y + pallet1.speed
+      
+  end
 
+  if love.keyboard.isDown("w") then
+    pallet2.y = pallet2.y - pallet2.speed
+    
+  elseif love.keyboard.isDown("s") then
+    pallet2.y = pallet2.y + pallet2.speed
+
+  end
+
+end
 
 function love.draw()
   
-  love.graphics.draw(spaceshipImage, posX, posY)
+  love.graphics.rectangle("line", pallet1.x, pallet1.y, pallet1.width, pallet1.height)
+  
+  love.graphics.rectangle("line", pallet2.x, pallet2.y, pallet2.width, pallet2.height)
 
 end
