6e380478a93785fbad974d6d45e5889c7777502f
Ivan Abraham <93773441+IvanAbraham@users.noreply.github.com>
2023-10-02 16:20:39 -0300

    Added ball and created limits for both pallets

--- Löve Pong/main.lua
+++ Löve Pong/main.lua
@@ -1,21 +1,37 @@
 function love.load()
    
+  screenWidth = 600
+  screenHeight = 400
+   
+  love.window.setMode(600, 400, {resizable = false, vsync = false})
+   
+  randnumber = love.math.random(0,1)
+   
   pallet1 = 
   {
-    x = 750,
-    y = 50,
-    width = 10,
-    height = 150,
-    speed = 10
+    x = screenWidth - 50,
+    y = screenHeight/2,
+    width = screenWidth/70,
+    height = screenHeight/5,
+    speed = 1
   }
     
   pallet2 =
   {
     x = 50,
-    y = 50,
-    width = 10,
-    height = 150,
-    speed = 10
+    y = screenHeight/2,
+    width = screenWidth/70,
+    height = screenHeight/5,
+    speed = 1
+  }
+  
+  ball=
+  {
+    posx = screenWidth/2,
+    posy = screenHeight/2,
+    radius = 10,
+    speed = 1,
+    vspeed = 1
   }
    
 end
@@ -23,22 +39,51 @@ end
 
 function love.update(dt)
   
-  if love.keyboard.isDown("up") then
-    pallet1.y = pallet1.y - pallet1.speed
+  if pallet1.y > 0 then
+  
+    if love.keyboard.isDown("up") then
+     
+     pallet1.y = pallet1.y - pallet1.speed
     
-  elseif love.keyboard.isDown("down") then
-    pallet1.y = pallet1.y + pallet1.speed
+    end
       
   end
-
-  if love.keyboard.isDown("w") then
-    pallet2.y = pallet2.y - pallet2.speed
     
-  elseif love.keyboard.isDown("s") then
-    pallet2.y = pallet2.y + pallet2.speed
-
+  if pallet1.y + pallet1.height <= screenHeight then
+    
+    if love.keyboard.isDown("down") then
+    
+      pallet1.y = pallet1.y + pallet1.speed
+        
+    end
+  
   end
+  
+  if pallet2.y > 0 then
+  
+    if love.keyboard.isDown("w") then
+      
+      pallet2.y = pallet2.y - pallet2.speed
 
+    end
+ 
+  end
+ 
+  if pallet2.y + pallet2.height <= screenHeight then
+ 
+    if love.keyboard.isDown("s") then
+    
+      pallet2.y = pallet2.y + pallet2.speed
+    
+    end
+ 
+ end
+ 
+ 
+  ball.posx = ball.posx + 1 * ball.speed
+  ball.posy = ball.posy + 1 * ball.vspeed
+
+  
 end
 
 function love.draw()
@@ -46,5 +91,7 @@ function love.draw()
   love.graphics.rectangle("line", pallet1.x, pallet1.y, pallet1.width, pallet1.height)
   
   love.graphics.rectangle("line", pallet2.x, pallet2.y, pallet2.width, pallet2.height)
+  
+  love.graphics.circle("line", ball.posx, ball.posy, ball.radius)
 
 end
