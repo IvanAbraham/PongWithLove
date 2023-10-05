bfe0f031bc765712ba9ed9121e7f444717edbe4c
IvanAbraham <ivan99.abraham@gmail.com>
2023-10-05 04:11:43 -0300

    Added colours and win/lose conditions

--- Löve Pong/main.lua
+++ Löve Pong/main.lua
@@ -9,7 +9,7 @@ function love.load()
    
   pallet1 = 
   {
-    x = screenWidth - 50,
+    x = screenWidth - 20,
     y = screenHeight/2,
     width = screenWidth/70,
     height = screenHeight/5,
@@ -18,7 +18,7 @@ function love.load()
     
   pallet2 =
   {
-    x = 50,
+    x = 20,
     y = screenHeight/2,
     width = screenWidth/70,
     height = screenHeight/5,
@@ -30,8 +30,8 @@ function love.load()
     posx = screenWidth/2,
     posy = screenHeight/2,
     radius = 5,
-    speed = 0.5,
-    vspeed = -0.5
+    speed = 1,
+    vspeed = 1
   }
    
   p1points = 0
@@ -40,111 +40,153 @@ function love.load()
   p1textpoints = string.char(p1points)
   p2textpoints = string.char(p2points)
   
+  p1win = false
+  p2win = false
+  
+  play = true
+  
 end
 
 
 function love.update(dt)
   
-  if pallet1.y > 0 then
+  if play == true then
   
-    if love.keyboard.isDown("up") then
-     
-     pallet1.y = pallet1.y - pallet1.speed
+    if pallet1.y > 0 then
     
-    end
+      if love.keyboard.isDown("up") then
+       
+       pallet1.y = pallet1.y - pallet1.speed
       
-  end
-    
-  if pallet1.y + pallet1.height <= screenHeight then
-    
-    if love.keyboard.isDown("down") then
-    
-      pallet1.y = pallet1.y + pallet1.speed
+      end
         
     end
-  
-  end
-  
-  if pallet2.y > 0 then
-  
-    if love.keyboard.isDown("w") then
       
-      pallet2.y = pallet2.y - pallet2.speed
-
-    end
- 
-  end
- 
-  if pallet2.y + pallet2.height <= screenHeight then
- 
-    if love.keyboard.isDown("s") then
-    
-      pallet2.y = pallet2.y + pallet2.speed
+    if pallet1.y + pallet1.height <= screenHeight then
+      
+      if love.keyboard.isDown("down") then
+      
+        pallet1.y = pallet1.y + pallet1.speed
+          
+      end
     
     end
- 
-  end
+    
+    if pallet2.y > 0 then
+    
+      if love.keyboard.isDown("w") then
+        
+        pallet2.y = pallet2.y - pallet2.speed
 
- 
-  if ball.posy + ball.radius == screenHeight or ball.posy - ball.radius == 0 then
+      end
    
-    ball.vspeed = ball.vspeed * -1
-  
-  end
- 
- if ball.posx + ball.radius >= pallet1.x or ball.posx - ball.radius <= pallet2.x + pallet2.width then
-    
-    if ball.posx + ball.radius >= pallet1.x then
-      
-      if ball.posy >= pallet1.y and ball.posy <= pallet1.y + pallet1.height then
-      
-      ball.posx = pallet1.x - ball.radius
-      
-      ball.speed = ball.speed * -1
-      
-      elseif ball.posx > screenWidth then
-      
-      ball.posx = screenWidth/2
-      ball.posy = screenHeight/2
-      p2points = p2points + 1
-      
-      end
-      
-    elseif ball.posx - ball.radius <= pallet2.x + pallet2.width then
-      
-      if ball.posy >= pallet2.y and ball.posy <= pallet2.y + pallet2.height then
-      
-      ball.posx = pallet2.x + pallet2.width + ball.radius
-      
-      ball.speed = ball.speed * -1
-      
-      elseif ball.posx < 0 then 
-      
-      ball.posx = screenWidth/2
-      ball.posy = screenHeight/2
-      p1points = p1points + 1
-      
-      end
-      
     end
+   
+    if pallet2.y + pallet2.height <= screenHeight then
+   
+      if love.keyboard.isDown("s") then
+      
+        pallet2.y = pallet2.y + pallet2.speed
+      
+      end
+   
+    end
+
+   
+    if ball.posy + ball.radius == screenHeight or ball.posy - ball.radius == 0 then
+     
+      ball.vspeed = ball.vspeed * -1
+    
+    end
+   
+   if ball.posx + ball.radius >= pallet1.x or ball.posx - ball.radius <= pallet2.x + pallet2.width then
+      
+      if ball.posx + ball.radius >= pallet1.x then
+        
+        if ball.posy >= pallet1.y and ball.posy <= pallet1.y + pallet1.height then
+        
+          ball.posx = pallet1.x - ball.radius
+          ball.speed = ball.speed * -1
+        
+        elseif ball.posx > screenWidth then
+        
+          ball.posx = screenWidth/2
+          ball.posy = screenHeight/2
+          p2points = p2points + 1
+          
+          if p2points == 3 then
+          
+            p2win = true
+            play = false
+            
+          end
+        
+        end
+        
+      elseif ball.posx - ball.radius <= pallet2.x + pallet2.width then
+        
+        if ball.posy >= pallet2.y and ball.posy <= pallet2.y + pallet2.height then
+        
+          ball.posx = pallet2.x + pallet2.width + ball.radius
+          ball.speed = ball.speed * -1
+          
+        elseif ball.posx < 0 then 
+          
+          ball.posx = screenWidth/2
+          ball.posy = screenHeight/2
+          p1points = p1points + 1
+         
+          if p1points == 3 then
+            
+            p1win = true
+            play = false
+            
+          end
+        
+        end
+        
+      end
+    
+    end
+    
+    ball.posx = ball.posx + 1 * ball.speed
+    ball.posy = ball.posy + 1 * ball.vspeed
   
   end
-  
-  ball.posx = ball.posx + 1 * ball.speed
-  ball.posy = ball.posy + 1 * ball.vspeed
-  
+
 end
 
 function love.draw()
+  if play == true then
+    
+    love.graphics.setColor(255, 0, 0)
+    
+    love.graphics.rectangle("line", pallet1.x, pallet1.y, pallet1.width, pallet1.height)
+    love.graphics.print("" .. p1points, screenWidth-10, 10)
+    
+    love.graphics.setColor(0, 50, 255)
+    
+    love.graphics.rectangle("line", pallet2.x, pallet2.y, pallet2.width, pallet2.height)
+    love.graphics.print("" .. p2points, 10, 10)
+    
+    love.graphics.setColor(255, 255, 255)
+    
+    love.graphics.circle("line", ball.posx, ball.posy, ball.radius)
   
-  love.graphics.rectangle("line", pallet1.x, pallet1.y, pallet1.width, pallet1.height)
-  
-  love.graphics.rectangle("line", pallet2.x, pallet2.y, pallet2.width, pallet2.height)
-  
-  love.graphics.circle("line", ball.posx, ball.posy, ball.radius)
-  
-  love.graphics.print("" .. p1points, 10, 10)
-  
-  love.graphics.print("" .. p1points, screenWidth-10, 10)
+  else 
 
+    if p1win then
+      
+      love.graphics.setColor(255, 0, 0)
+      love.graphics.print("Player 1 Won!", screenWidth/2, screenHeight/2)
+    
+    else
+      
+      love.graphics.setColor(0, 50, 255)
+      love.graphics.print("Player 2 Won!", screenWidth/2, screenHeight/2)
+    
+    end
+  
+  end
+  
 end
